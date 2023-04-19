e6ca164680966aedeb3a3ad8906fa63e62dc2f47
Yubie <ayoub04bd@gmail.com>
2023-04-19 14:47:45 +0000

    fixed tailwindcss errors

--- webpack.config.js
+++ webpack.config.js
@@ -72,11 +72,7 @@ Encore
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
 
-    .enablePostCssLoader((options) => {
-        options.postcssOptions = {
-            config: './postcss.config.js'
-        }
-    })
+    .enablePostCssLoader()
 ;
 
 module.exports = Encore.getWebpackConfig();
