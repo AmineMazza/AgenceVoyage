b8019f26a6e119ec4f1459c85384006e6b3e15f2
Yubie <ayoub04bd@gmail.com>
2023-04-25 11:07:27 +0100

    registre style done

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -24,12 +24,26 @@ class RegistrationType extends AbstractType
                 'required' => false,
                 'mapped' => false
             ))
-            ->add('email', TextType::class)
+            ->add('email', TextType::class, [
+                'attr' => ['class' => 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer',
+                            'id' => 'floating_email',
+                            'name' => 'floating_email',
+                            'type' => 'email',
+                            'placeholder' => " "                        
+
+            ]
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password',
+                            'class' => 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer',
+                            'id' => 'floating_password',
+                            'name' => 'floating_password',
+                            'type' => 'password',
+                            'placeholder' => " "                         
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -40,29 +54,75 @@ class RegistrationType extends AbstractType
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
+
                 ],
             ])
             ->add('agence', TextType::class, [
                 'required' => false,
+                'attr' =>['class' => 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer',
+                            'id' => 'floating_agence',
+                            'name' => 'floating_agence',
+                            'type' => 'text',
+                            'placeholder' => " "                        
+
+            ],
             ])
-            ->add('nom', TextType::class)
-            ->add('prenom', TextType::class)
-            ->add('telephone_mobile', TextType::class)
+            ->add('nom', TextType::class, [
+                'attr' => ['class' => 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer',
+                           'id' => 'floating_nom',
+                           'name' => 'floating_nom',
+                           'type' => 'text',
+                           'placeholder' => " "  
+                ]
+            ])
+            ->add('prenom', TextType::class, [
+                'attr' => ['class' => 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer',
+                           'id' => 'floating_prenom',
+                           'name' => 'floating_prenom',
+                           'type' => 'text',
+                           'placeholder' => " "  
+                ]
+            ])
+            ->add('telephone_mobile', TextType::class, [
+                'attr' => ['class' => 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer',
+                           'id' => 'floating_tel',
+                           'name' => 'floating_tel',
+                           'type' => 'tel',
+                           'placeholder' => " "  
+                ]
+            ])
             ->add('telephone_fixe', TextType::class, [
                 'required' => false,
+                'attr' => ['class' => 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer',
+                           'id' => 'floating_fixe',
+                           'name' => 'floating_fixe',
+                           'type' => 'tel',
+                           'placeholder' => " "  
+                ]
             ])
             ->add('adresse', TextType::class, [
                 'required' => false,
+                'attr' => ['class' => 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer',
+                            'id' => 'floating_adresse',
+                            'name' => 'floating_adresse',
+                            'type' => 'text',
+                            'placeholder' => " "                        
+
+            ],
             ])
             ->add('type_abonnement', ChoiceType::class, [
                 'choices' => [
+                    'type d\'abonnement: ' => null,
                     'free' => 'free',
                     'premium' => 'premium',
                 ],
-                'expanded' => true,
+                'expanded' => false,
                 'multiple' => false,
             ])
-            ->add('submit', SubmitType::class)
+            ->add('submit', SubmitType::class, [
+                'attr' => ['class' => 'absolute h-12 left-96 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800']
+            ]
+            )
         ;
     }
 
