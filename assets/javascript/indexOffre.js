d8af3dc102b61cfeb0edb1161ed1dafbd30c1250
abdelaziz <afquir1abdelaziz@gmail.com>
2023-05-05 09:16:43 +0100

    add js of popup offre

--- assets/javascript/indexOffre.js
+++ assets/javascript/indexOffre.js
@@ -1,1 +1,18 @@
+const clickableImages = document.querySelectorAll('#clickableImage');
+const imageContainer = document.getElementById('displayImage');
+const closePop=document.getElementById('ClosePopUp');
+console.log(closePop);
+clickableImages.forEach((image) => {
+  image.addEventListener('click', () => {
+    const imageUrl = image.getAttribute('src');
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', imageUrl);
+    newImage.setAttribute('id', 'imagepop');
+    imageContainer.appendChild(newImage);
+  });
+});
 
+closePop.addEventListener('click',function(){
+    const image=document.getElementById('imagepop');
+    imageContainer.removeChild(image);
+})
