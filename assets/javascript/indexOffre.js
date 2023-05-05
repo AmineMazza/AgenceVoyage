7612832d9af62db0520d1639790904ea3b212171
abdelaziz <afquir1abdelaziz@gmail.com>
2023-05-05 09:38:00 +0100

    fix javascript popup

--- assets/javascript/indexOffre.js
+++ assets/javascript/indexOffre.js
@@ -1,7 +1,7 @@
 const clickableImages = document.querySelectorAll('#clickableImage');
 const imageContainer = document.getElementById('displayImage');
 const closePop=document.getElementById('ClosePopUp');
-console.log(closePop);
+const popupContainer=document.getElementById('popup-modal');
 clickableImages.forEach((image) => {
   image.addEventListener('click', () => {
     const imageUrl = image.getAttribute('src');
@@ -16,3 +16,14 @@ closePop.addEventListener('click',function(){
     const image=document.getElementById('imagepop');
     imageContainer.removeChild(image);
 })
+popupContainer.addEventListener('click',function(){
+    const image=document.getElementById('imagepop');
+    imageContainer.removeChild(image);
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "Escape" || event.keyCode === 27) {
+        const image=document.getElementById('imagepop');
+        imageContainer.removeChild(image);
+    }
+  });
