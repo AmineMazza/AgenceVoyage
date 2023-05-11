68b6928bf8d9cd1033d0aa4376894c8a65576e8b
Yubie <ayoub04bd@gmail.com>
2023-05-11 09:37:02 +0100

    image path fixed

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -47,24 +47,31 @@ class RegistrationController extends AbstractController
             // logo
             $logo=$request->files->get('registration')['logo'];
             //  dd($request->files->get('registration')['logo']);
-            if ($logo) {
-                $newFileName = uniqid() . '.' . $logo->guessExtension();
-
-                try {
-                    $logo->move(
-                        $this->getParameter('kernel.project_dir') . '/public/uploads/AgenceLogo',
-                        $newFileName
-                    );
-                } catch (FileException $e) {
-                    return new Response($e->getMessage());
-                }
-            
-                $agent->setLogo('/public/uploads/AgenceLogo/' . $newFileName);
+            if ($logo){
+                $originalFilename = pathinfo($logo->getClientOriginalName(), PATHINFO_FILENAME);
+                
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$logo->guessExtension();
+                
+                
+                $agent->setLogo('/assets/agentLogo/'.$newFilename);
+                
             }
             $user->setAgent($agent);
             $bool = $userApiService->AddUser($user);
-            if($bool)
+            if($bool){
+                if($logo){
+                    try {
+                        $logo->move(
+                            $this->getParameter('logo_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        throw new Exception($e);
+                    }
+                }
                 return $this->redirectToRoute('app_login');
+            }
             else{
                 return $this->render('registration/register.html.twig', [
                     'registrationForm' => $form->createView(),
