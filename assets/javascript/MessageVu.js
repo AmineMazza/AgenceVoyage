ae88c8183a6cc2edd6965e9ea940dfda97ad52d5
abdelaziz <afquir1abdelaziz@gmail.com>
2023-05-19 14:43:24 +0100

    add nom message

--- assets/javascript/MessageVu.js
+++ assets/javascript/MessageVu.js
@@ -1,10 +1,12 @@
 const VoirBtn = document.querySelectorAll('#VoirBTN');
 const MegContainer=document.getElementById("messagecontainer");
-
+const nameMessage=document.getElementById('nameMessage');
 VoirBtn.forEach((btn) => {
     btn.addEventListener('click', function () {
         const id = btn.closest('tr').querySelector('.id-column').textContent;
         const message= btn.closest('tr').querySelector('.message-column').textContent;
+        const nom= btn.closest('tr').querySelector('.nom-column').textContent;
+        nameMessage.innerHTML=nom;
         MegContainer.innerText=message;
         console.log('Clicked on ID:', id);
     });
