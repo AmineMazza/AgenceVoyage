e645c7137c1fa71e3b6e99a17663eaaa7ea91cb6
abdelaziz <afquir1abdelaziz@gmail.com>
2023-05-19 10:40:34 +0100

    fix contact activeLink

--- assets/javascript/base.js
+++ assets/javascript/base.js
@@ -14,8 +14,8 @@ if(activeLink.startsWith("http://127.0.0.1/home")){
     home.classList.add("text-gray-900");
     contact.classList.add("text-gray-900");
 }else if(activeLink.startsWith("http://127.0.0.1/contact")){
-    console.log("hey1")
-    contact.classList.add("text-blue-600");
     offre.classList.add("text-gray-900");
     home.classList.add("text-gray-900");
+    contact.classList.remove('text-gray-900');
+    contact.classList.add("text-blue-600");
 }
