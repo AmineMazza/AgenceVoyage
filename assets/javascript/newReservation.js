6d8b38b9150b95634d82f20cef6b96f35673062c
Yubie <ayoub04bd@gmail.com>
2023-05-23 16:00:12 +0100

    styled

--- assets/javascript/newReservation.js
+++ assets/javascript/newReservation.js
@@ -73,30 +73,33 @@ function isCommercialEvent(){
 function numVoyageurEvent(){
     document.querySelector("#voyageurInfo").innerHTML = ``;
     let voyageurHTML = `
-    <div id="reservation_voyageurs___name__" class="flex">\
-        <div>
-            <label for="reservation_voyageurs___name___nom" class="required">Nom</label>
-            <input type="text" id="reservation_voyageurs___name___nom" name="reservation[voyageurs][__name__][nom]" required="required" maxlength="50" />
+    <div id="reservation_voyageurs___name__" class="space-y-3 border rounded-lg p-6 relative z-10 mt-6">\
+        <div class="absolute text-sm font-medium text-gray-500 dark:text-gray-400 duration-300 left-4 -top-3 px-2 z-50 bg-white">Voyageur N&deg; </div>
+        <div class="relative">
+            <input type="text" id="reservation_voyageurs___name___nom" name="reservation[voyageurs][__name__][nom]" required="required" maxlength="50" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+            <label for="reservation_voyageurs___name___nom" class="required absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Nom</label>
+        </div>
+        <div class="relative">
+            <input type="text" id="reservation_voyageurs___name___prenom" name="reservation[voyageurs][__name__][prenom]" required="required" maxlength="50" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
+            <label for="reservation_voyageurs___name___prenom" class="required absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Prenom</label>
+        </div>
+        <div class="relative">
+            <input type="text" id="reservation_voyageurs___name___cin" name="reservation[voyageurs][__name__][cin]" maxlength="20" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
+            <label for="reservation_voyageurs___name___cin" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Cin</label>
+        </div>
+        <div class="relative">
+            <input type="text" id="reservation_voyageurs___name___num_passport" name="reservation[voyageurs][__name__][num_passport]" maxlength="100" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
+            <label for="reservation_voyageurs___name___num_passport" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Num passport</label>
         </div>
         <div>
-            <label for="reservation_voyageurs___name___prenom" class="required">Prenom</label>
-            <input type="text" id="reservation_voyageurs___name___prenom" name="reservation[voyageurs][__name__][prenom]" required="required" maxlength="50" />
-        </div>
-        <div>
-            <label for="reservation_voyageurs___name___cin">Cin</label>
-            <input type="text" id="reservation_voyageurs___name___cin" name="reservation[voyageurs][__name__][cin]" maxlength="20" />
-        </div>
-        <div>
-            <label for="reservation_voyageurs___name___num_passport">Num passport</label>
-            <input type="text" id="reservation_voyageurs___name___num_passport" name="reservation[voyageurs][__name__][num_passport]" maxlength="100" />
-        </div>
-        <div>
-            <select id="reservation_voyageurs___name___prix" name="reservation[voyageurs][__name__][prix]">
+            <label for="reservation_voyageurs___name___prix" class="block mb-0 text-sm font-small text-gray-500 dark:text-white">Choix de chambre:</label>
+            <select id="reservation_voyageurs___name___prix" name="reservation[voyageurs][__name__][prix]" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 ${choicesChambre}
             </select>
         </div>
         <div>
-            <select id="reservation_voyageurs___name___pension" name="reservation[voyageurs][__name__][pension]">
+            <label for="reservation_voyageurs___name___pension" class="block mb-0 text-sm font-small text-gray-500 dark:text-white">Choix de pension:</label>
+            <select id="reservation_voyageurs___name___pension" name="reservation[voyageurs][__name__][pension]" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 ${choicesPension}
             </select>
         </div>
