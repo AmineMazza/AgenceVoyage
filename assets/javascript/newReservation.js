5cad49efa6f5ef4c603a21036c0e5ceebb23faf8
abdelaziz <afquir1abdelaziz@gmail.com>
2023-05-24 15:26:02 +0100

    edit js reservation

--- assets/javascript/newReservation.js
+++ assets/javascript/newReservation.js
@@ -14,33 +14,35 @@ let reservationRemarque=document.getElementById('reservation_remarque');
 let AjoutBtn=document.getElementById('AjoutBtn');
 let remplieIsValid=true;
 function displayBtn(){
-    reservationRemarque.disabled = true;
-    reservationRemarque.style.backgroundColor = "#f2f2f2";
-    reservationRemarque.style.color = "#999999";
-    //** */
-    RemplissezBtn.disabled = true;
-    RemplissezBtn.style.backgroundColor = "#f2f2f2";
-    RemplissezBtn.style.color = "#999999";
-    RemplissezBtn.style.cursor = "not-allowed";
-    //** */
-    isCommercial.disabled = true;
-    isCommercial.style.backgroundColor = "#f2f2f2";
-    isCommercial.style.color = "#999999";
-    //** */
-    AjoutBtn.disabled = true;
-    AjoutBtn.style.backgroundColor = "#f2f2f2";
-    AjoutBtn.style.color = "#999999";
-    AjoutBtn.style.cursor = "not-allowed";
+    "text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white";
+    "mb-6 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500";
+        reservationRemarque.disabled = true;
+        reservationRemarque.classList.add("text-gray-400","bg-transparent",'cursor-not-allowed',"hover:bg-gray-200","hover:text-gray-900","rounded-lg","text-sm","p-1.5","ml-auto ","inline-flex","items-center","dark:hover:bg-gray-600", "dark:hover:text-white");
+            //** */
+        RemplissezBtn.disabled = true;
+        RemplissezBtn.style.backgroundColor = "#f2f2f2";
+        RemplissezBtn.style.color = "#999999";
+        RemplissezBtn.style.cursor = "not-allowed";
+         //** */
+        isCommercial.disabled = true;
+        isCommercial.style.backgroundColor = "#f2f2f2";
+        isCommercial.style.color = "#999999";
+        isCommercial.style.cursor = "not-allowed";
+       //** */
+       AjoutBtn.disabled = true;
+       AjoutBtn.classList.add('text-white','bg-blue-400','dark:bg-blue-500','cursor-not-allowed','font-medium','rounded-lg','text-sm','px-5','py-2.5','text-center');
+
 }
 window.addEventListener('load', function() {
 let num = parseInt(numVoyageur.value);
-if(num === 0 || isNaN(num) ){
+if(num <= 0 || isNaN(num) ){
     displayBtn();
 }
 else{
     reservationRemarque.disabled = false;
     reservationRemarque.style.backgroundColor = "";
     reservationRemarque.style.color = "";
+    reservationRemarque.style.cursor = "";
     RemplissezBtn.disabled = false;
     RemplissezBtn.style.backgroundColor = "";
     RemplissezBtn.style.color = "";
@@ -48,6 +50,9 @@ else{
     isCommercial.disabled = false;
     isCommercial.style.backgroundColor = "";
     isCommercial.style.color = "";
+    isCommercial.style.cursor = "";
+    reservationRemarque.classList.remove("text-gray-400","bg-transparent",'cursor-not-allowed',"hover:bg-gray-200","hover:text-gray-900","rounded-lg","text-sm","p-1.5","ml-auto ","inline-flex","items-center","dark:hover:bg-gray-600", "dark:hover:text-white");
+
 }
 });
 const closeRemplier=document.getElementById('closeRemplier');
@@ -75,18 +80,12 @@ SauvegarderBtn.addEventListener('click',function(){
 
     if(remplieIsValid==true){
         AjoutBtn.disabled = false;
-        AjoutBtn.style.backgroundColor = "";
-        AjoutBtn.style.color = "";
-        AjoutBtn.style.cursor = "";
-        errorReservation.innerText="";
-
+       AjoutBtn.classList.remove('bg-blue-400','dark:bg-blue-500','cursor-not-allowed');
     }
     else{
-        errorReservation.innerText = "Erreur, veuillez remplir les chomp de premier voyageur.";
+        errorReservation.innerText = "veuillez remplir les chomp de premier voyageur.";
         AjoutBtn.disabled = true;
-        AjoutBtn.style.backgroundColor = "#f2f2f2";
-        AjoutBtn.style.color = "#999999";
-        AjoutBtn.style.cursor = "not-allowed";
+        AjoutBtn.classList.add('text-white','bg-blue-400','dark:bg-blue-500','cursor-not-allowed','font-medium','rounded-lg','text-sm','px-5','py-2.5','text-center'); 
     }
 });
 //** */
@@ -117,18 +116,12 @@ closeRemplier.addEventListener('click',function(){
     });
     if(remplieIsValid==true){
         AjoutBtn.disabled = false;
-        AjoutBtn.style.backgroundColor = "";
-        AjoutBtn.style.color = "";
-        AjoutBtn.style.cursor = "";
-        errorReservation.innerText="";
-
+       AjoutBtn.classList.remove('bg-blue-400','dark:bg-blue-500','cursor-not-allowed');
     }
     else{
-        errorReservation.innerText = "Erreur, veuillez remplir les chomp de premier voyageur.";
+        errorReservation.innerText = "veuillez remplir les chomp de premier voyageur.";
         AjoutBtn.disabled = true;
-        AjoutBtn.style.backgroundColor = "#f2f2f2";
-        AjoutBtn.style.color = "#999999";
-        AjoutBtn.style.cursor = "not-allowed";
+        AjoutBtn.classList.add('text-white','bg-blue-400','dark:bg-blue-500','cursor-not-allowed','font-medium','rounded-lg','text-sm','px-5','py-2.5','text-center'); 
     }
 
 })
@@ -201,11 +194,11 @@ function numVoyageurEvent(){
     <div id="reservation_voyageurs___name__" class="space-y-3 border rounded-lg p-6 relative z-10 mt-6">\
         <div class="absolute text-sm font-medium text-gray-500 dark:text-gray-400 duration-300 left-4 -top-3 px-2 z-50 bg-white">Voyageur N&deg; </div>
         <div class="relative">
-            <input type="text" id="reservation_voyageurs___name___nom" name="reservation[voyageurs][__name__][nom]" required="required" maxlength="50" class="reservationInput block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+            <input type="text" id="reservation_voyageurs___name___nom" name="reservation[voyageurs][__name__][nom]"  maxlength="50" class="reservationInput block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
             <label for="reservation_voyageurs___name___nom" class="required absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Nom</label>
         </div>
         <div class="relative">
-            <input type="text" id="reservation_voyageurs___name___prenom" name="reservation[voyageurs][__name__][prenom]" required="required" maxlength="50" class="reservationInput block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
+            <input type="text" id="reservation_voyageurs___name___prenom" name="reservation[voyageurs][__name__][prenom]" maxlength="50" class="reservationInput block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
             <label for="reservation_voyageurs___name___prenom" class="required absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Prenom</label>
         </div>
         <div class="relative">
@@ -218,13 +211,13 @@ function numVoyageurEvent(){
         </div>
         <div>
             <label for="reservation_voyageurs___name___prix" class="block mb-0 text-sm font-small text-gray-500 dark:text-white">Choix de chambre:</label>
-            <select id="reservation_voyageurs___name___prix" name="reservation[voyageurs][__name__][prix]" class="reservationInputChampre bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <select id="reservation_voyageurs___name___prix" name="reservation[voyageur][__name__][prix]" class="reservationInputChampre bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 ${choicesChambre}
             </select>
         </div>
         <div>
             <label for="reservation_voyageurs___name___pension" class="block mb-0 text-sm font-small text-gray-500 dark:text-white">Choix de pension:</label>
-            <select id="reservation_voyageurs___name___pension" name="reservation[voyageurs][__name__][pension]" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <select id="reservation_voyageurs___name___pension" name="reservation[voyageur][__name__][pension]" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 ${choicesPension}
             </select>
         </div>
@@ -232,29 +225,14 @@ function numVoyageurEvent(){
     `;
 
     let num = parseInt(numVoyageur.value);
-    if(num === 0 || isNaN(num) ){
-        reservationRemarque.disabled = true;
-        reservationRemarque.style.backgroundColor = "#f2f2f2";
-        reservationRemarque.style.color = "#999999";
-            //** */
-        RemplissezBtn.disabled = true;
-        RemplissezBtn.style.backgroundColor = "#f2f2f2";
-        RemplissezBtn.style.color = "#999999";
-        RemplissezBtn.style.cursor = "not-allowed";
-         //** */
-        isCommercial.disabled = true;
-        isCommercial.style.backgroundColor = "#f2f2f2";
-        isCommercial.style.color = "#999999";
-       //** */
-        AjoutBtn.disabled = true;
-        AjoutBtn.style.backgroundColor = "#f2f2f2";
-        AjoutBtn.style.color = "#999999";
-        AjoutBtn.style.cursor = "not-allowed";
+    if(num <= 0 || isNaN(num) ){
+        displayBtn();
     }
     else{
         reservationRemarque.disabled = false;
         reservationRemarque.style.backgroundColor = "";
         reservationRemarque.style.color = "";
+        reservationRemarque.style.cursor = "";
         RemplissezBtn.disabled = false;
         RemplissezBtn.style.backgroundColor = "";
         RemplissezBtn.style.color = "";
@@ -262,6 +240,7 @@ function numVoyageurEvent(){
         isCommercial.disabled = false;
         isCommercial.style.backgroundColor = "";
         isCommercial.style.color = "";
+        isCommercial.style.cursor = "";
     }
     for(i = 0; i<num; i++){
         document.querySelector("#voyageurInfo").innerHTML += voyageurHTML.replace(/__name__/g , i);
